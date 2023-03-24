7f96feb812f7352a20c0833770258d25d3135870
Manipushpak <manipushpakmandwade@gmail.com>
2023-03-24 16:45:28 +0530

    completed

---
+++ Products/Products/Controllers/ProductController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Products.Repository;
+using Products.Models;
+using PagedList.Mvc;
+using PagedList;
+
+namespace Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        public ActionResult IndexProd(int ? i)
+        {
+            ProductRepository prod = new ProductRepository();
+            ModelState.Clear();
+            return View(prod.GetAllProducts() .ToPagedList(i??1,10));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }    
+  
+
+        [HttpPost]
+        public ActionResult Create(ProductModel pd)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    ProductRepository prod = new ProductRepository();
+
+                    if (prod.AddProd(pd))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET:  
+        public ActionResult Edit(int id)
+        {
+            ProductRepository prod = new ProductRepository();
+            return View(prod.GetAllProducts().Find(pd => pd.ProductId == id));
+        }
+
+        // POST:  
+        [HttpPost]
+        public ActionResult Edit(int id, ProductModel obj)
+        {
+            try
+            {
+                ProductRepository prod = new ProductRepository();
+                prod.UpdateProduct(obj);
+                return RedirectToAction("IndexProd");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                ProductRepository prod = new ProductRepository();
+                if (prod.DeleteProduct(id))
+                {
+                    ViewBag.AlertMsg = "Employee details deleted successfully";
+
+                }
+                return RedirectToAction("IndexProd");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+  
+	}
+}
---
+++ Products/Products/Repository/ProductRepository.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Products.Models;
+using System.Data;
+
+namespace Products.Repository
+{
+    public class ProductRepository
+    {
+        private SqlConnection con;
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["ProductDatabase"].ToString();
+            con = new SqlConnection(constr);
+        }
+
+        public List<ProductModel> GetAllProducts()
+        {
+            connection();
+            List<ProductModel> ProdList = new List<ProductModel>();
+            SqlCommand com = new SqlCommand("Select P.ProductId, P.ProductName, C.CategoryId, C.CategoryName from SampleDB.dbo.Product P join SampleDB.dbo.Category C on C.CategoryId = P.CategoryId;", con);
+            com.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                ProdList.Add(
+
+                    new ProductModel
+                    {
+                        ProductId = Convert.ToInt32(dr["ProductId"]),
+                        ProductName = Convert.ToString(dr["ProductName"]),
+                        CategoryId = Convert.ToInt32(dr["CategoryId"]),
+                        CategoryName = Convert.ToString(dr["CategoryName"])
+                    }
+                    );
+            }
+
+            return ProdList;
+        }
+
+        internal bool AddProd(ProductModel pd)
+        {
+            connection();
+            SqlCommand com = new SqlCommand("Insert into SampleDB.dbo.Product (ProductId, ProductName, CategoryId) Values (@ProductID, @ProductName, @CategoryId)", con);
+            com.CommandType = CommandType.Text;
+            com.Parameters.AddWithValue("@ProductID", pd.ProductId);
+            com.Parameters.AddWithValue("@ProductName", pd.ProductName);
+            com.Parameters.AddWithValue("@CategoryId", pd.CategoryId);
+            
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }    
+        }
+
+
+        internal bool UpdateProduct(ProductModel pd)
+        {
+            connection();
+            SqlCommand com = new SqlCommand("Update Product Set ProductId =@ProductID, ProductName=@ProductName,CategoryId= @CategoryId where ProductId = @ProductID", con);
+
+            com.CommandType = CommandType.Text;
+            com.Parameters.AddWithValue("@ProductID", pd.ProductId);
+            com.Parameters.AddWithValue("@ProductName", pd.ProductName);
+            com.Parameters.AddWithValue("@CategoryId", pd.CategoryId);
+            
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteProduct(int Id)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("Delete from Product where  ProductId =@ProductID", con);
+
+            com.CommandType = CommandType.Text;
+            com.Parameters.AddWithValue("@ProductID", Id);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }    
+    }
+}
